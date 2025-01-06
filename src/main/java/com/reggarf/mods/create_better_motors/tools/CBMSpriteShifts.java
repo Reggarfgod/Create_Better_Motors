e592f391675e1a8fdb8ed7a95734498141f2a855
Anoop Singh <Reggerfgod@gmail.com>
2025-01-07 00:24:20 +0530

    SPRITESHAFT UPDATE

--- src/main/java/com/reggarf/mods/create_better_motors/tools/CBMSpriteShifts.java
+++ src/main/java/com/reggarf/mods/create_better_motors/tools/CBMSpriteShifts.java
@@ -6,28 +6,18 @@ import com.simibubi.create.foundation.block.connected.CTSpriteShiftEntry;
 import com.simibubi.create.foundation.block.connected.CTSpriteShifter;
 import com.simibubi.create.foundation.block.connected.CTType;
 
+import static com.simibubi.create.foundation.block.connected.AllCTTypes.*;
+
 
 public class CBMSpriteShifts {
-
-	public static final CTSpriteShiftEntry
-			VOID_CASING = omni("void_casing"),
-			VOID_STEEL_SCAFFOLD = horizontal("scaffold/void_steel_scaffold"),
-			VOID_STEEL_SCAFFOLD_INSIDE = horizontal("scaffold/void_steel_scaffold_inside");
-
-	private static CTSpriteShiftEntry omni(String name) {
-		return getCT(AllCTTypes.OMNIDIRECTIONAL, name);
-	}
-
-	private static CTSpriteShiftEntry horizontal(String name) {
-		return getCT(AllCTTypes.HORIZONTAL, name);
-	}
 
 	private static CTSpriteShiftEntry getCT(CTType type, String blockTextureName) {
 		return getCT(type, blockTextureName, blockTextureName);
 	}
-
 	private static CTSpriteShiftEntry getCT(CTType type, String blockTextureName, String connectedTextureName) {
-		return CTSpriteShifter.getCT(type, Create_better_motors.asResource("block/" + blockTextureName), Create_better_motors.asResource("block/" + connectedTextureName + "_connected"));
+		return CTSpriteShifter.getCT(type,
+				Create_better_motors.asResource("block/" + blockTextureName),
+				Create_better_motors.asResource("block/" + connectedTextureName + "_connected"));
 	}
 
 }
